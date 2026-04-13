31dd188b6a3795b3d42293923518168a4c94d94d
sitiulawu <sitiulawulandari@gmail.com>
2026-04-13 10:50:50 +0700

    update lll

---
+++ auth.js
@@ -0,0 +1,6 @@
+//firewal sederhana untuk mencegah akses aplikasi//tanpa login terlebih dahulu 
+const isLogin = localStorage.getItem("isLogin")
+
+if (!isLogin !== "true") {
+  window.location.herf = "login.html"
+}
--- main.js
+++ main.js
@@ -8,7 +8,9 @@ import {
   deleteDoc,
   updateDoc,
   doc,
-  getDoc
+  getDoc,
+  query,
+  where
 } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-firestore.js";
 
 // GANTI DENGAN FIREBASE CONFIG ANDA
@@ -24,6 +26,38 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app)
 const tanamanCollection = collection(db, "tanaman")
+
+export async function Login(){
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+  
+const q = query (
+  collection(db, "users"),
+  where("username", "==", username),
+  where("password", "==", password)
+)
+  const querySnapshot = await getDocs(collection(db, "users"));
+
+  let found = false;
+
+  querySnapshot.forEach((doc) => {
+    const data = doc.data();
+
+    if (data.username === username && data.password === password) {
+      found = true;
+    }
+  });
+
+  if (found) {
+    document.getElementById("status").innerText = "Login berhasil";
+    // redirect
+    window.location.href = "index.html";
+  } else {
+    document.getElementById("status").innerText = "Username atau password salah";
+}
+  
+}
+
 
 //fungsi untum menampilkan daftar film dan drama
 export async function daftartanaman() {
@@ -151,4 +185,4 @@ export async function hapustanaman(id){
     
     }
     
-    
+  
